a7b4c5fc150545199df309bb1f6f8f2e190beba2
be3rodnov <be3rodnov@gmail.com>
2025-06-08 22:59:50 +0300

    update 08.06.2025 часть 3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,26 @@
     });
   });
 }
+$(document).ready(function () {
+  blockMenu();
+});
+$(window).resize(function () {
+  blockMenu();
+});
+function blockMenu() {
+  if (window.innerWidth < 761) {
+    $(".block-m-menu__menu").append($(".menu__list"));
+  } else {
+    $(".block-header__menu").append($(".menu__list"));
+  }
+}
+
+$(".block-header__menu-toggle").on("click", function () {
+  $(this).toggleClass("active");
+  $(".block-m-menu").toggleClass("active");
+});
+$("body").on("click", ".block-m-menu .menu__item", function (e) {
+  $(".block-header__menu-toggle").removeClass("active");
+  $(".block-m-menu").removeClass("active");
+  console.log("1");
+});
